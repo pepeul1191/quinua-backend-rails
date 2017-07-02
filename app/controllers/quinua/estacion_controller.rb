5628b3cfa74b8616e324e70267903d05789e7ba6
Pepe Valdivia <jvaldivia@softweb.pe>
2017-07-01 22:47:34 -0500

    Controlador estructura el JSON de respuesta

--- app/controllers/quinua/estacion_controller.rb
+++ app/controllers/quinua/estacion_controller.rb
@@ -8,7 +8,21 @@ class Quinua::EstacionController < ApplicationController
 	def detalle
 		id = params[:id]
 		estacion = Quinua::Estacion.new
-		rpta = estacion.detalle(id).to_json
-		render :plain => rpta
+		estaciones = estacion.detalle(id)
+
+		sensores =[]
+		for  estacion in estaciones
+			temp = Hash.new 
+			temp[:nombre_sensor] = estacion[:nombre_sensor]
+			temp[:desc_instrumento] = estacion[:desc_instrumento]
+			temp[:des_tipo] = estacion[:des_tipo]
+			sensores.push(temp)
+		end
+		rpta = Hash.new
+		rpta[:nombre_estacion] = estaciones[0][:nombre_estacion]
+		rpta[:descripcion] = estaciones[0][:descripcion]
+		rpta[:sensores] = sensores
+
+		render :plain => rpta.to_json
 	end
 end
