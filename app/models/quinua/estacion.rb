619af8c4bed4384a8e29f8ac644d2f22fbc3016f
Pepe Valdivia <jvaldivia@softweb.pe>
2017-07-13 15:28:11 -0500

    Correción en DML y SQL para obtener el detalle de una estación

--- app/models/quinua/estacion.rb
+++ app/models/quinua/estacion.rb
@@ -19,7 +19,7 @@ class Quinua::Estacion
                SELECT IE.nombre_estacion, IE.descripcion, II.nombre_sensor, II.desc_instrumento, MTT.des_tipo FROM inve_estacion IE 
 				INNER JOIN inve_estacion_instru IEI ON IE.ide_estacion = IEI.ide_estacion 
 				INNER JOIN inve_instru II ON II.ide_sensor = IEI.ide_sensor 
-				INNER JOIN mae_tablatipo_tipo MTT ON MTT.ide_tipo = II.tipo_unidad_medida
+				INNER JOIN mae_tablatipo_tipo MTT ON MTT.ide_tipo = II.fk_tipo_unidad_medida 
 				WHERE IEI.ide_estacion = ?', id].to_a
         @db.disconnect
         rpta 
