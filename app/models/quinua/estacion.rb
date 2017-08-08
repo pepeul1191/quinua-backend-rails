26f0df8f486caa6380282a72d90dee2334ed084e
Pepe Valdivia <jvaldivia@softweb.pe>
2017-08-08 15:54:21 -0500

    Base de datos de prueba SQLite modificada

--- app/models/quinua/estacion.rb
+++ app/models/quinua/estacion.rb
@@ -3,12 +3,12 @@
 # -*- coding: UTF-8 -*-
 
 class Quinua::Estacion
-  	def initialize
-  		# tabla : inve_estacion
-  	    @db = Databases.db_quinua
-  	end
+  def initialize
+  	# tabla : inve_estacion
+      @db = Databases.db_quinua
+  end
 
-  	def listar
+    def listar
         rpta = @db[:inve_estacion].to_a
         @db.disconnect
         rpta 
--- app/models/quinua/sensor.rb
+++ app/models/quinua/sensor.rb
@@ -10,17 +10,16 @@ class Quinua::Sensor
 
     def historico(id, fecha_inicio, fecha_fin)
         # sqlite3
-        #q = 'SELECT strftime("%d",  fec_med) as dia, strftime("%m",  fec_med) as mes, strftime("%Y",  fec_med) as anio, AVG(valor_med) AS medicion 
-        #        FROM inve_instru_dato 
-        #        WHERE fec_med > strftime("' + fecha_inicio +'") AND fec_med < strftime("' + fecha_fin+ '") AND ide_sensor = ' + id + '
-        #        GROUP BY dia;'
-
-        # postgres
-        q = "SELECT to_char(fec_med, 'YY') AS anio,to_char(fec_med, 'MM') AS mes, to_char(fec_med, 'DD') AS dia, AVG(valor_med) AS medicion 
+        q = 'SELECT strftime("%d",  fec_med) as dia, strftime("%m",  fec_med) as mes, strftime("%Y",  fec_med) as anio, AVG(valor_med) AS medicion 
                 FROM inve_instru_dato 
-                WHERE fec_med > '" + fecha_inicio + "' AND fec_med < '" + fecha_fin + "' AND ide_sensor = " + id + " 
-                GROUP BY dia, mes, anio 
-                ORDER BY anio, mes, dia;"
+                WHERE fec_med > strftime("' + fecha_inicio +'") AND fec_med < strftime("' + fecha_fin+ '") AND ide_sensor = ' + id + '
+                GROUP BY dia;'
+        # postgres
+        #q = "SELECT to_char(fec_med, 'YY') AS anio,to_char(fec_med, 'MM') AS mes, to_char(fec_med, 'DD') AS dia, AVG(valor_med) AS medicion 
+                #FROM inve_instru_dato 
+                #WHERE fec_med > '" + fecha_inicio + "' AND fec_med < '" + fecha_fin + "' AND ide_sensor = " + id + " 
+                #GROUP BY dia, mes, anio 
+                #ORDER BY anio, mes, dia;"
 
         rpta = @db[q]
         @db.disconnect
